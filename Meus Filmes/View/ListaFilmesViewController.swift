a9ea97ec95cb4dabda3fb8e5b0e6430b550ae58b
Igor Vilar <igorvilar@outlook.com>
2019-11-26 22:53:20 -0300

    removido controller e storyboard de projeto inicial e criado controller de lista de filmes e storyboard de filmes

---
+++ Meus Filmes/View/ListaFilmesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ListaFilmesViewController.swift
+//  Meus Filmes
+//
+//  Created by Igor Vilar on 26/11/19.
+//  Copyright © 2019 Igor Vilar. All rights reserved.
+//
+
+import UIKit
+
+class ListaFilmesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
